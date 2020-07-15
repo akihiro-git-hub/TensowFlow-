457dd0e4c4bd6fd32b58e7e98ac89f772342c76c
akihiro-git-hub <odoru3657x@outlook.jp>
2020-07-15 11:54:49 +0900

    階層構造を構築しました

--- fashion-mnist.py
+++ fashion-mnist.py
@@ -6,6 +6,10 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+print('ラベルのクラスに対応するものを作成しておく')
+class_name = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
 print('------------------------------')
 
 print('以下データ内容の確認')
@@ -43,3 +47,21 @@ print('-----------------------------')
 
 print('データの中に何が入っているのかを表示してみる')
 
+#subplotの3つめの引数はインデックスの指定。左上を１といて横に1つ動くとインデックスが１つ大きくなる。
+#ticksはラベルの間隔の事。noneだとそのままの値を出す。
+#cmapのbinaryはグレースケール
+plt.figure(figsize=(10,10))
+for i in range(25):
+  plt.subplot(5,5,i+1)
+  plt.xticks([])
+  plt.yticks([])
+  plt.imshow(train_images[i],cmap=plt.cm.binary)
+  plt.xlabel(class_name[train_labels[i]])
+plt.show()
+
+print('層の構築を行っていく')
+
+model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)),tf.keras.layers.Dense(128,activation='relu'),
+tf.keras.layers.Dense(10,activation='softmax')])
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
